6e53e009aba3d7720cc2aab5e6215dcd10a0a70d
Shauren <shauren.trinity@gmail.com>
2018-09-18 23:37:45 +0200

    Core/Players: Updated pvp talent implementation to bfa

---
+++ sql/updates/characters/master/2018_09_18_00_characters.sql
@@ -0,0 +1,6 @@
+TRUNCATE `character_pvp_talent`;
+ALTER TABLE `character_pvp_talent`
+  CHANGE `talentId` `talentId0` int(10) unsigned NOT NULL AFTER `guid`,
+  ADD `talentId1` int(10) unsigned NOT NULL AFTER `talentId0`,
+  ADD `talentId2` int(10) unsigned NOT NULL AFTER `talentId1`,
+  ADD `talentId3` int(10) unsigned NOT NULL AFTER `talentId2`;
